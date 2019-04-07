17ec7720c153dfc84db628b9e7de9f05314ec5f0
Felipe Siqueira <flipggs@gmail.com>
2019-04-07 07:18:53 -0300

    add API e class lib

---
+++ APIAlturas/Controllers/ConversorAlturasController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alturas;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIAlturas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConversorAlturasController : Controller
+    {
+        // GET api/values
+        [HttpGet("PesParaMetros/{pes}")]
+        public object Get(double pes)
+        {
+            return new 
+            {
+                alturaPes = pes,
+                alturaMetros = ConversorAlturas.PesParaMetros(pes)
+            };
+        }
+    }
+}
